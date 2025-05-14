9252440c10208310a6367eeb6d7a4c1125c9f14b
Tom LaMarre <tlamarre91+github@gmail.com>
2025-05-13 19:13:48 -0500

    add some options

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "sonokai" } },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -29,8 +29,9 @@ require("lazy").setup({
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
+    keymaps = false,
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "sonokai" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
